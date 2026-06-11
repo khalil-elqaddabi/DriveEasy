aa8a4ec55bc1396b36047925cbbd7d3dbfa53ddb
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-06-11 22:45:43 +0100

    feat: create car model

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    //
+    protected $fillable = [
+
+        'brand',
+        'model',
+        'year',
+        'seats',
+        'price_per_day',
+        'status',
+        'image',
+        'descriptions',
+
+    ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function features()
+    {
+        return $this->belongsToMany(Feature::class);
+    }
 }
